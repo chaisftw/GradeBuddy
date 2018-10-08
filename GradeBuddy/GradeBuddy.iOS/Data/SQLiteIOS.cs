27972279385b9ac721606c12090799b18f0056eb
ChaisB <n9937951@qut.edu.au>
2018-10-08 17:31:15 +1000

    Added database structure

---
+++ GradeBuddy/GradeBuddy.iOS/Data/SQLiteIOS.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using Xamarin.Forms;
+using GradeBuddy.Data;
+using GradeBuddy.iOS.Data;
+using System.IO;
+[assembly: Dependency(typeof(SQLiteIOS))]
+
+namespace GradeBuddy.iOS.Data
+{
+    public class SQLiteIOS : ISQLite
+    {
+
+        public SQLiteIOS() { }
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var filename = "gradeDatabase.db3";
+            var documentPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var libraryPath = Path.Combine(documentPath, "..", "Library");
+            var path = Path.Combine(libraryPath, filename);
+            var connection = new SQLite.SQLiteConnection(path);
+            return connection;
+        }
+
+    }
+}
---
+++ GradeBuddy/GradeBuddy/Data/DBItemManager.cs
@@ -0,0 +1,142 @@
+﻿using GradeBuddy.Models;
+using SQLite;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace GradeBuddy.Data
+{
+    public class DBItemManager
+    {
+        private static object locker = new object();
+        private SQLiteConnection database;
+
+        public DBItemManager()
+        {
+            this.database = DependencyService.Get<ISQLite>().GetConnection();
+            this.database.CreateTable<UnitModel>();
+            this.database.CreateTable<AssessmentModel>();
+            this.database.CreateTable<AssessmentItemModel>();
+        }
+
+        public IEnumerator<UnitModel> GetDBUnits()
+        {
+            lock (locker)
+            {
+                if (this.database.Table<UnitModel>().Count() == 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return this.database.Table<UnitModel>().GetEnumerator();
+                }
+
+            }
+        }
+
+        public int SaveDBUnit(UnitModel item)
+        {
+            lock (locker)
+            {
+                if (item.UnitID != 0)
+                {
+                    this.database.Update(item);
+                    return item.UnitID;
+                }
+                else
+                {
+                    return this.database.Insert(item);
+                }
+            }
+        }
+
+        public int DeleteDBUnit(int id)
+        {
+            lock (locker)
+            {
+                return this.database.Delete<UnitModel>(id);
+            }
+        }
+
+        public IEnumerator<AssessmentModel> GetDBAssessments()
+        {
+            lock (locker)
+            {
+                if (this.database.Table<AssessmentModel>().Count() == 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return this.database.Table<AssessmentModel>().GetEnumerator();
+                }
+
+            }
+        }
+
+        public int SaveDBAssessment(AssessmentModel item)
+        {
+            lock (locker)
+            {
+                if (item.AssessmentID != 0)
+                {
+                    this.database.Update(item);
+                    return item.AssessmentID;
+                }
+                else
+                {
+                    return this.database.Insert(item);
+                }
+            }
+        }
+
+        public int DeleteDBAssessment(int id)
+        {
+            lock (locker)
+            {
+                return this.database.Delete<AssessmentModel>(id);
+            }
+        }
+
+        public IEnumerator<AssessmentItemModel> GetDBAssessmentItems()
+        {
+            lock (locker)
+            {
+                if (this.database.Table<AssessmentItemModel>().Count() == 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return this.database.Table<AssessmentItemModel>().GetEnumerator();
+                }
+
+            }
+        }
+
+        public int SaveDBAssessmentItem(AssessmentItemModel item)
+        {
+            lock (locker)
+            {
+                if (item.AssessmentItemID != 0)
+                {
+                    this.database.Update(item);
+                    return item.AssessmentItemID;
+                }
+                else
+                {
+                    return this.database.Insert(item);
+                }
+            }
+        }
+
+        public int DeleteDBAssessmentItem(int id)
+        {
+            lock (locker)
+            {
+                return this.database.Delete<AssessmentItemModel>(id);
+            }
+        }
+
+    }
+}
