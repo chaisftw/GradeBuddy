0ae2d00efcd3dab940d899897773ca53e4e9a25f
ChaisB <n9937951@qut.edu.au>
2018-10-16 14:09:57 +1000

    Added viewmodel

---
+++ GradeBuddy/GradeBuddy/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using System;
+using System.ComponentModel;
+
+
+namespace GradeBuddy
+{
+    public class ViewModel : ViewModelBase
+    {
+        public static NavigationManager navigationManager;
+
+        public ViewModel()
+        {
+            navigationManager = NavigationManager.Instance;
+
+        }
+    }
+}
+
