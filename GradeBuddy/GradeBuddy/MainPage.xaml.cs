9f63a4da9ceb92261719787556771b4bb19d9c8b
Dale <dale.roy@connect.qut.edu.au>
2018-10-24 21:58:44 +1000

    Beginning of ContentPages

--- GradeBuddy/GradeBuddy/MainPage.xaml.cs
+++ GradeBuddy/GradeBuddy/MainPage.xaml.cs
@@ -4,14 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using GradeBuddy.Data;
+using GradeBuddy.Models;
 
 namespace GradeBuddy
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+        }
+
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new SubjectPage
+                {
+                    BindingContext = e.SelectedItem as UnitModel
+                });
+            }
+        }
+
+        async void AddSubject(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new AddSubjectPage
+            //{
+            //    BindingContext = new UnitModel()
+            //});
         }
     }
 }
